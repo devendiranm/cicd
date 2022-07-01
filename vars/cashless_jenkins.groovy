a702cf9f9aa72fce5667d2baa9bc3f5718eb1ca7
devendiranm <108513514+devendiranm@users.noreply.github.com>
2022-07-01 19:38:22 +0530

    Update cashless_jenkins.groovy

--- vars/cashless_jenkins.groovy
+++ vars/cashless_jenkins.groovy
@@ -3,232 +3,231 @@ def call(Map pipelineParams)
 	pipeline
 	{
 		agent
+  			{
+    				label 'Linux'
+    			}
+    		options
   		{
-    		label 'Linux'
-    	}
-    	options
-  		{
-    		buildDiscarder(
-            	logRotator(
-                	daysToKeepStr: '60',   // Build Records
-                	artifactDaysToKeepStr: '60'  //Artifacts from builds older than this number of days will be deleted, but the logs, history, reports, etc for the build will be kept
-            		)
+    			buildDiscarder(
+            			logRotator(
+                		daysToKeepStr: '60',   // Build Records
+                			artifactDaysToKeepStr: '60'  //Artifacts from builds older than this number of days will be deleted, but the logs, history, reports, etc for the build will be kept
+            			)
         		)
-        	timeout(50)
-        	timestamps()
-        	disableConcurrentBuilds()
-    	}
+        		timeout(50)
+        		timestamps()
+        		disableConcurrentBuilds()
+    		}
   		environment 
 		{ 
-    		projectArtifactId = 'ArtifactId'
-    		projectGroupId = 'GroupId'
-    		projectVersion = 'Version'
-     		artifactType = 'Packaging'
-          	bitbucket_repo = "gal-cashless"
-          	branch_type = 'branch_type'
-          	branch = 'branch'
-      	}
+    			projectArtifactId = 'ArtifactId'
+    			projectGroupId = 'GroupId'
+    			projectVersion = 'Version'
+     			artifactType = 'Packaging'
+          		bitbucket_repo = "gal-cashless"
+          		branch_type = 'branch_type'
+          		branch = 'branch'
+      		}
 		stages
   		{
-          	stage('Get Branch Type')
-      		{
-        		steps
-          		{
-        			sh 'git branch -r >branch1.txt'
-                  	sh 'cat branch1.txt'
-              		sh 'cat branch1.txt | awk -F\'/\' \'{print $2}\' >branch_type.txt'
-                  	sh 'cat branch1.txt | awk -F\'/\' \'{print $3}\' >branch.txt'
-                  	
-              		script
-              		{
-              			branch_type = readFile('branch_type.txt').trim()
-                  		echo "Branch Type is #${branch_type}#"
-                      	branch = readFile('branch.txt').trim()
-                      	echo "Branch name is #${branch}#"
-                	}
-            	}
-       		}
-    		stage("Build and Package")
-      		{
-        		steps
-          		{
-              		echo "Branch Name is : ${env.BRANCH_NAME}"
-                  	echo "Branch Type is : ${branch_type}"
-                  	echo "Branch name is : ${branch}"
-                  	echo (pipelineParams.nexus_url)
-                 	script
+        		stage('Get Branch Type')
+      			{
+        			steps
+          			{
+        				sh 'git branch -r >branch1.txt'
+                	  		sh 'cat branch1.txt'
+              				sh 'cat branch1.txt | awk -F\'/\' \'{print $2}\' >branch_type.txt'
+                  			sh 'cat branch1.txt | awk -F\'/\' \'{print $3}\' >branch.txt'
+                  			script
+              				{
+              					branch_type = readFile('branch_type.txt').trim()
+                  				echo "Branch Type is #${branch_type}#"
+                      				branch = readFile('branch.txt').trim()
+                      				echo "Branch name is #${branch}#"
+                			}
+            			}
+       			}
+    			stage("Build and Package")
+      			{
+        			steps
+          			{
+              				echo "Branch Name is : ${env.BRANCH_NAME}"
+                  			echo "Branch Type is : ${branch_type}"
+                  			echo "Branch name is : ${branch}"
+                  			echo (pipelineParams.nexus_url)
+                 			script
 					{
 						pom = readMavenPom file: "pom.xml"
-                    	projectArtifactId = pom.getArtifactId()
+                    				projectArtifactId = pom.getArtifactId()
 						projectGroupId = pom.getGroupId()
 						projectVersion = pom.getVersion()
 						artifactType = pom.getPackaging()
-                	}
-//              		sh "export MAVEN_OPTS=-Xmx2048m"
-            		sh "mvn clean install"
-//                  	sh '''cd templates
+                			}
+//              			sh "export MAVEN_OPTS=-Xmx2048m"
+            				sh "mvn clean install"
+//                  			sh '''cd templates
 //						jar -cvf templates.jar *.*'''
-              		echo 'Build completed'
-            	}
-       		}
-//     		stage('Sonarqube master')
+              				echo 'Build completed'
+            			}
+       			}
+//     			stage('Sonarqube master')
 //			{
-//            	when
-//              	{
-//                	branch 'master'
-//                }
+//            			when
+//              		{
+//                			branch 'master'
+//                		}
 //				environment
-//     			{
+//     				{
 //					SCANNER_HOME = tool 'SonarqubeScanner'
 //				}
 //				steps
-//     			{
-//                	withSonarQubeEnv('SonarQube')
-//         			{
+//     				{
+//      	       		   	withSonarQubeEnv('SonarQube')
+//         				{
 //						sh '''$SCANNER_HOME/bin/sonar-scanner \
 //						-Dsonar.projectKey=CASHLESS_MASTER \
 //						-Dsonar.java.binaries=target/classes/ \
-//             			-Dsonar.sources=src/main/java/'''
+//             					-Dsonar.sources=src/main/java/'''
 //					}
-//                }
-//            }
-//          	stage('Sonarqube release')
+//                		}
+//            		}
+//          		stage('Sonarqube release')
 //			{
-//            	when
-//              	{
-//                	branch 'release/*'
-//                }
+//            			when
+//              		{
+//                			branch 'release/*'
+//                		}
 //				environment
-//     			{
+//     				{
 //					SCANNER_HOME = tool 'SonarqubeScanner'
 //				}
 //				steps
-//     			{
-//                	withSonarQubeEnv('SonarQube')
-//         			{
+//     				{
+//                			withSonarQubeEnv('SonarQube')
+//         				{
 //						sh '''$SCANNER_HOME/bin/sonar-scanner \
 //						-Dsonar.projectKey=CASHLESS_RELEASE \
 //						-Dsonar.java.binaries=target/classes/ \
-//             			-Dsonar.sources=src/main/java/'''
+//             					-Dsonar.sources=src/main/java/'''
 //					}
-//                }
-//            }
-//          	stage('Sonarqube development')
+//                		}
+//            		}
+//          		stage('Sonarqube development')
 //			{
-//            	when
-//              	{
-//                	branch 'develop'
-//                }
+//            			when
+//              		{
+//                			branch 'develop'
+//                		}
 //				environment
-//     			{
+//     				{
 //					SCANNER_HOME = tool 'SonarqubeScanner'
 //				}
 //				steps
-//     			{
-//                	withSonarQubeEnv('SonarQube')
-//         			{
+//     				{
+//                			withSonarQubeEnv('SonarQube')
+//         				{
 //						sh '''$SCANNER_HOME/bin/sonar-scanner \
 //						-Dsonar.projectKey=CASHLESS_DEVELOPMENT \
 //						-Dsonar.java.binaries=target/classes/ \
-//             			-Dsonar.sources=src/main/java/'''
+//             					-Dsonar.sources=src/main/java/'''
 //					}
-//                }
-//            }
-//          	stage('Sonarqube feature')
+//                		}
+//            		}
+//          		stage('Sonarqube feature')
 //			{
-//            	when
-//              	{
-//                	branch 'feature/*'
-//                }
+//            			when
+//              		{
+//               	 		branch 'feature/*'
+//                		}
 //				environment
-//     			{
+//     				{
 //					SCANNER_HOME = tool 'SonarqubeScanner'
 //				}
 //				steps
-//     			{
-//                	withSonarQubeEnv('SonarQube')
-//         			{
+//     				{
+//                			withSonarQubeEnv('SonarQube')
+//         				{
 //						sh '''$SCANNER_HOME/bin/sonar-scanner \
 //						-Dsonar.projectKey=CASHLESS_FEATURE \
 //						-Dsonar.java.binaries=target/classes/ \
-//             			-Dsonar.sources=src/main/java/'''
+//             					-Dsonar.sources=src/main/java/'''
 //					}
-//                }
-//            }
-//*            stage("Uploading master WAR file to Nexus")
+//                		}
+//            		}
+//*            		stage("Uploading master WAR file to Nexus")
 			{
-            	when
+            			when
 				{
-                	branch 'master'
-            	}
-        		steps
+                			branch 'master'
+            			}
+        			steps
 				{
 					nexusArtifactUploader(
 						artifacts: [[artifactId: "${env.BRANCH_NAME}", classifier: '', file: "target/${projectArtifactId}-${projectVersion}.${artifactType}", type: "${artifactType}"],
-							[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "pom.xml", type: "pom" ],
-                      		[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "templates/templates.jar", type: "jar" ]],
-                		credentialsId: 'd9f3ff8c-9dd2-4233-856f-db2921861c1a',
-                		groupId: "${bitbucket_repo}",
-                		nexusUrl: (pipelineParams.nexus_url),
-                		nexusVersion: 'nexus3',
-                		protocol: 'http',
-                		repository: (pipelineParams.nexus_prod_repo),
-                      	version: "${env.BRANCH_NAME}-cashless"
-					)
-           		}
-            }
-          	stage("Uploading Relese WAR file to Nexus")
+						[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "pom.xml", type: "pom" ],
+                      				[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "templates/templates.jar", type: "jar" ]],
+                				credentialsId: 'd9f3ff8c-9dd2-4233-856f-db2921861c1a',
+                				groupId: "${bitbucket_repo}",
+                				nexusUrl: (pipelineParams.nexus_url),
+                				nexusVersion: 'nexus3',
+                				protocol: 'http',
+                				repository: (pipelineParams.nexus_prod_repo),
+                      				version: "${env.BRANCH_NAME}-cashless"
+						)
+           			}
+            		}
+          		stage("Uploading Relese WAR file to Nexus")
 			{
-            	when
+            			when
 				{
-                	branch 'release/*'
-	           	}
-              	steps
-              	{
-        			nexusArtifactUploader(
+                			branch 'release/*'
+	           		}
+              			steps
+              			{
+        				nexusArtifactUploader(
 						artifacts: [[artifactId: "${branch_type}", classifier: '', file: "target/${projectArtifactId}-${projectVersion}.${artifactType}", type: "${artifactType}"],
-							[artifactId: "${branch_type}",classifier: '', file: "pom.xml", type: "pom" ],
-                      		[artifactId: "${branch_type}",classifier: '', file: "templates/templates.jar", type: "jar" ]],
-                		credentialsId: 'd9f3ff8c-9dd2-4233-856f-db2921861c1a',
-                		groupId: "${bitbucket_repo}",
-                		nexusUrl: (pipelineParams.nexus_url),
-                		nexusVersion: 'nexus3',
-                		protocol: 'http',
-                		repository: (pipelineParams.nexus_prod_repo),
-                      	version: "${branch}-cashless"
-					)
-                }
-            }
-          	stage("Uploading development WAR file to Nexus")
+						[artifactId: "${branch_type}",classifier: '', file: "pom.xml", type: "pom" ],
+                      				[artifactId: "${branch_type}",classifier: '', file: "templates/templates.jar", type: "jar" ]],
+                				credentialsId: 'd9f3ff8c-9dd2-4233-856f-db2921861c1a',
+                				groupId: "${bitbucket_repo}",
+                				nexusUrl: (pipelineParams.nexus_url),
+                				nexusVersion: 'nexus3',
+                				protocol: 'http',
+                				repository: (pipelineParams.nexus_prod_repo),
+                      				version: "${branch}-cashless"
+						)
+                		}
+            		}
+          		stage("Uploading development WAR file to Nexus")
 			{
-            	when
+            			when
 				{
-                	branch 'develop'
-            	}
-        		steps
+                			branch 'develop'
+            			}
+        			steps
 				{
 					nexusArtifactUploader(
 						artifacts: [[artifactId: "${env.BRANCH_NAME}", classifier: '', file: "target/${projectArtifactId}-${projectVersion}.${artifactType}", type: "${artifactType}"],
-							[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "pom.xml", type: "pom" ],
-                      		[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "templates/templates.jar", type: "jar" ]],
-                		credentialsId: 'd9f3ff8c-9dd2-4233-856f-db2921861c1a',
-                		groupId: "${bitbucket_repo}",
-                		nexusUrl: (pipelineParams.nexus_url),
-                		nexusVersion: 'nexus3',
-                		protocol: 'http',
-                		repository: (pipelineParams.nexus_nonprod_repo),
-                      	version: "${env.BRANCH_NAME}-cashless"
-					)
-           		}
-//*         }
-          	stage('CleanWorkspace')
-    		{
-            	steps
-              	{
-        			cleanWs()
-        			echo 'Cleaned workspace'
-                }
-            }
-    	}
+						[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "pom.xml", type: "pom" ],
+                      				[artifactId: "${env.BRANCH_NAME}",classifier: '', file: "templates/templates.jar", type: "jar" ]],
+                				credentialsId: 'd9f3ff8c-9dd2-4233-856f-db2921861c1a',
+                				groupId: "${bitbucket_repo}",
+                				nexusUrl: (pipelineParams.nexus_url),
+                				nexusVersion: 'nexus3',
+                				protocol: 'http',
+                				repository: (pipelineParams.nexus_nonprod_repo),
+                      				version: "${env.BRANCH_NAME}-cashless"
+						)
+           			}
+*//         		}
+          		stage('CleanWorkspace')
+    			{
+            			steps
+              			{
+        				cleanWs()
+        				echo 'Cleaned workspace'
+                		}
+            		}
+    		}
 	}
 }
